017e534549f165236a78ed287a74f45d14fc5e2c
kaoru-528 <origami.kaoru@gmail.com>
2024-05-12 16:47:06 +0900

    Prepare for develop branch

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,9 @@
 class MainController < ApplicationController
   def main
   end
+  def post_data
+    response = HTTParty.post('http://localhost:8000', body: { dt: params[:dt]})
+  end
   def summary
     response = HTTParty.get('http://localhost:8000')
     @summary_data = response.parsed_response
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "main#main"
+  post "/post_data", to: "main#post_data"
   get "/summary", to: "main#summary"
   resources :main
 end
